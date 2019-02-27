c9a10a7623e4edbdd017bc5d80b76a4c333704cf
Albertas Dvirnas <retardedshrimp@gmail.com>
2019-02-27 11:43:31 +0100

    Fix bug with background detection

--- src/MATLAB/+CBT/+Consensus/+Import/+Helper/gen_barcode_data.m
+++ src/MATLAB/+CBT/+Consensus/+Import/+Helper/gen_barcode_data.m
@@ -5,19 +5,19 @@ function [barcodeGenData] = gen_barcode_data(alignedKymo)
 
     import OptMap.MoleculeDetection.EdgeDetection.approx_main_kymo_molecule_edges;
     [leftEdgeIdxs, rightEdgeIdxs, alignedKymoMoleculeMask] = approx_main_kymo_molecule_edges(alignedKymo, edgeDetectionSettings);
-    
+
     leftEdgeIdx = round(nanmean(leftEdgeIdxs));
     rightEdgeIdx = round(nanmean(rightEdgeIdxs));
-    
+
     if rightEdgeIdx <= leftEdgeIdx
         disp('ah');
     end
-    
+
     % Determine indices for rotated barcode with background cropped out
     barcodeIdxs = leftEdgeIdx:rightEdgeIdx;
     rawBarcode = nanmean(alignedKymo, 1);
-    
-    bgIndices = true(1, size(alignedKymo, 1));
+
+    bgIndices = true(1, size(alignedKymo, 2));
     bgIndices(barcodeIdxs) = 0;
 
     nonBarcodeVals = alignedKymo(:, bgIndices);
@@ -25,11 +25,11 @@ function [barcodeGenData] = gen_barcode_data(alignedKymo)
     nonBarcodeDistFit = fitdist(nonBarcodeVals, 'Normal');
     barcodeGenData.bgMeanApprox =  nonBarcodeDistFit.mu;
     barcodeGenData.bgStdApprox = nonBarcodeDistFit.sigma;
-    
+
     rawBg = nanmean(rawBarcode(bgIndices));
-  
+
     barcodeGenData.rawBarcode = rawBarcode(barcodeIdxs);
-    
+
     barcodeGenData.rawBarcodeLeftEdgeIndex = leftEdgeIdx;
     barcodeGenData.rawBarcodeRightEdgeIndex = rightEdgeIdx;
     barcodeGenData.rawBg = rawBg;
