d5ea287fbeab53e707e29a82273dfe9ee2bdb658
Albertas Dvirnas <retardedshrimp@gmail.com>
2019-02-27 10:51:28 +0100

    Remove +Hca since it's included in separate rep

--- src/MATLAB/+CBT/+Hca/+UI/edit_kymographs_fun.m
+++
@@ -1,8 +0,0 @@
-function [ hcaSessionStruct ] = edit_kymographs_fun( hcaSessionStruct,kymoStructs,kymoNames )
-    % edit_kymographs_fun 
-    % this just puts kymo's in the hca structure
-    kymoStructs = cellfun(@(tl) tl.unalignedKymo,kymoStructs,'UniformOutput', false);     
-    hcaSessionStruct.unalignedKymos = kymoStructs;
-    hcaSessionStruct.names = kymoNames;       
-end
-
