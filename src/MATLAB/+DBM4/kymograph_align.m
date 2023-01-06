8e8da4b9fcd8738f7f39e1a56e29327b6c88ff82
albertas albertas <albdvir@gmail.com>
2023-01-06 16:30:14 +0100

    save kymos as a multiframe tif

--- src/MATLAB/+DBM4/kymograph_align.m
+++ src/MATLAB/+DBM4/kymograph_align.m
@@ -1,4 +1,5 @@
-function [alignedKymo, stretchFactorsMat, shiftAlignedKymo, alignedMask, shiftAlignedKymoMask, shiftingVect, alignmentSuccessTF] = kymograph_align(unalignedKymo, skipPrealignTF, kymoMask)
+function [alignedKymo, stretchFactorsMat, alignedKymoF, alignedMask, shiftAlignedKymoMask]...
+    = kymograph_align(unalignedKymo, kymoMask)
     % Kymograph-align - non-recursively aligns a kymograph
     %
     % Inputs:
@@ -33,12 +34,14 @@ function [alignedKymo, stretchFactorsMat, shiftAlignedKymo, alignedMask, shiftAl
     % Authors:
     %	HN, SQ, AD
 
+%     if nargin < 2
+%         skipPrealignTF = false;
+%     end
     if nargin < 2
-        skipPrealignTF = false;
-    end
-    if nargin < 3
         kymoMask = true(size(unalignedKymo));
     end
+    
+    tic
     
     % %   forceEdgesTF (optional; defaults to false)
     % %     true if forcing alignment near edges
@@ -47,13 +50,22 @@ function [alignedKymo, stretchFactorsMat, shiftAlignedKymo, alignedMask, shiftAl
     % end
     % edgeFeatureAlignmentBuffer = 10;
     
+    % STEP1: shift align
     % shift align might fail for molecules touching edges.
     shiftAlignedKymo = unalignedKymo;
     shiftAlignedKymoMask = kymoMask;
     
+    unalignedKymo(~kymoMask) = nan;
     
-    n = 50;
-    maxFeatureMovementPx = 5;
+    
+    maxFeatureMovementPx = 50; % maximum feature movement frame to frame
+        edgePx = 10;
+%                 edgePx = maxFeatureMovementPx;
+
+
+    n = sum(~isnan(unalignedKymo(1,:)))-4*maxFeatureMovementPx; % feature size
+
+
     ftMovement = -maxFeatureMovementPx:maxFeatureMovementPx;
     
     % The (maximum) number of features to look for "k"
@@ -61,257 +73,238 @@ function [alignedKymo, stretchFactorsMat, shiftAlignedKymo, alignedMask, shiftAl
     nM = ceil(minFgMaskWidth /n);
     shiftMat = zeros(size(kymoMask,1)-1,nM);
     
-    for i=1:size(kymoMask,1)-1
-        % non-zero pixels of this row
-        pixels = find(kymoMask(i,:));
-        % split into n parts
-        out = reshape([pixels';nan(mod(-numel(pixels'),n),1)],n,[]);
-        for j=1:nM
-            k = out(:,j);
-            % compare to next row
-            ftC = unalignedKymo(i,k(~isnan(k)));
-            % now compare to neighboring positions on next row
-            try
-                scores = arrayfun(@(x) sum((ftC-unalignedKymo(1+i,k+x)).^2),ftMovement);
-            catch
-                scores = nan(1,length(ftMovement)); % treat better last case
-            end
-            [a,b] = min(scores);
-            shiftMat(i,j) = ftMovement(b);
-%             cumsum(shiftMat(6,:))
-        end
-%         R = reshape(pixels,maxNumFeaturesSoughtK,[]);
-%         feat1 = 
+    kymo2 =    [nan(size(unalignedKymo,1),maxFeatureMovementPx) unalignedKymo nan(size(unalignedKymo,1),100)];
+%     bitmask2 = [zeros(size(kymoMask,1),maxFeatureMovementPx) kymoMask zeros(size(kymoMask,1),100)];
+%     
+%     corScore = zeros(1,size(kymoMask,1)-1);
+    
+
+
+    %%
+    
+    % create forward and backward vectors, the average then gives a more
+    % accurate location
+    shiftF = zeros(1,size(kymoMask,1));
+    shiftB = zeros(1,size(kymoMask,1));
+
+       % define forward/backward for more accurate 
+    for i = 1:size(kymoMask,1)-1
+
         
+        pixelsF = find(kymoMask(i+1,:));
+        pixelsB = find(kymoMask(end-i,:));
+        
+        kF = pixelsF(edgePx:end-edgePx+1);
+        kB = pixelsB(edgePx:end-edgePx+1);
+        
+        vF = unalignedKymo(i+1,kF);
+        vB = unalignedKymo(end-i,kB);
+        
+        scoresF = arrayfun(@(x) nanmean((vF-kymo2(i,maxFeatureMovementPx+kF+x)).^2),ftMovement);
+        scoresB = arrayfun(@(x) nanmean((vB-kymo2(end-i+1,maxFeatureMovementPx+kB+x)).^2),ftMovement);
+
+        [valueF, sF] = min(scoresF);
+        [valueB, sB] = min(scoresB);
+
+        shiftF(i+1) =   shiftF(i)+ftMovement(sF);
+        shiftB(end-i) =  shiftB(end-i+1)+ftMovement(sB);
     end
-    % cumsums gives approximate estimate of shift for that row
-        cumsumMat = cumsum(shiftMat,2);
-    figure,plot(cumsumMat(:,end)/length(pixels))
-    figure,plot(cumsum(cumsumMat(:,end)/length(pixels)))
-%     
-%     sets.psfnm = 300;
-%     sets.nmPerPixel = 110;
-%     
-%     optics.logSigma = sets.psfnm / sets.nmPerPixel;
-%     n = ceil(6 * optics.logSigma);
-%     n = n + 1 - mod(n, 2);
-%     filt = fspecial('log',[1 n],  optics.logSigma);
-%     logim = imfilter(unalignedKymo,filt);
-%     
-%     findpeaks(-logim(1,:)
-% 
-%         thedges = imbinarize(logim, 0);
-%         
-%         thedges = imclose(thedges, true(3));
-% %         se1 = strel('line',3,0)
-% %         thedges = imdilate(thedges,se1);
-% %         
-% %         thedges(1:end,[ 1 end]) = 1; % things around the boundary should also be considered
-%         thedges([ 1 end],1:end) = 1;
-%         
-%         
-%         % for each feature, cut pixels deviating from the central line
-% 
-%         [B, L] = bwboundaries(thedges, 'holes');
-% 
-%         [~, Gdir] = imgradient(logim);
-%         
-%         % todo: score for each position based on imgradient
-%         
-%         stat = @(h) mean(h); % This should perhaps be given from the outside
-%         
-%         longFeats = cellfun(@(x) size(x,1) >= sets.numPts,B);
-%         B = B(longFeats);
-%         
-%         % now hist of B will have two peaks
-% %         closedImg = imclose(B{14},strel('disk',30));
-% %         k =14;
-%     
-% 
-% %         [counts, binlocation] = imhist(B{14}(:,2));  %plus whatever option you used for imhist
-% % [sortedcount, indices] = sort(count);    %sort your histogram
-% % peakvalues = sortedcount(1:3)              %highest 3 count in the histogram
-% % peaklocations = binlocation(indices(1:3))  %respective image intensities for these peaks
-% 
-% 
-%         meh = zeros(1, length(B));
-% 
-% %         % find center
-% %         posX = cellfun(@(x) mean(x(:,2)),B);
-% %         for k = 1:length(B)% Filter out any regions with artifacts in them
-% % 
-% % 
-% %         end
-% %         
-% 
-%         for k = 1:length(B)% Filter out any regions with artifacts in them
-%             [N,edges] = histcounts(B{k}(:,2));
-%             bcenters = (edges(2:end)+edges(1:end-1))/2;
-%             [Ypk,Xpk] = findpeaks([0 N 0],'SortStr','desc','Npeaks',2);
-%             % 
-%             posEdges = bcenters(Xpk-1);
-% 
-%             % allow a few pixs variation
-%             pL = min(posEdges)-3;  pR = max(posEdges)+3;
-% 
-%             tooWide = (B{k}(:,2) < pL)+(B{k}(:,2) > pR);
-%             B{k}(logical(tooWide),:) = [];
-%             meh(k) = edge_score(B{k}, logim, Gdir, 5, stat); %how many points along the gradient to take?
-%          end
-%         
-%         %
-%         acc  = zeros(1, length(B));
-%         l = zeros(1, length(B));
-%         w = zeros(1, length(B));
-%         for k = 1:length(B)% Filter any edges with lower scores than lim
-%             [acc(k),l(k),w(k)] = mol_filt(B{k}, meh(k), sets.minScoreLambda, inf, [sets.minLambdaLen sets.maxLambdaLen], [1 sets.maxLambdaWidth]); % width depends on psf
-%         end
-%         
-%         
-%         potLambda = find(acc==1);
-%         posXlambda = zeros(1,length(potLambda));
-%         posYlambda =  zeros(length(potLambda),2);
-%         posMax =  zeros(1,length(potLambda));
-%         for j=1:length(potLambda)
-%             posXlambda(j) = round(mean(B{potLambda(j)}(:,2)));
-%             posYlambda(j,:) = [min(B{potLambda(j)}(:,1)) max(B{potLambda(j)}(:,1))];
-%             posMax(j) = round(mean(posYlambda(j,:)));            
-%         end
-%         
-%         
-%     
-%     % laplacian of gaussian
-% %     if not(skipPrealignTF)
-% %         import OptMap.KymoAlignment.NRAlign.pre_nralign_shift_align;
-% %         [shiftAlignedKymo, shiftingVect] = pre_nralign_shift_align(shiftAlignedKymo);
-% %         import OptMap.KymoAlignment.NRAlign.shift_rows;
-% %         shiftAlignedKymoMask = shift_rows(shiftAlignedKymoMask, shiftingVect);
-% %     else
-% %         shiftingVect = zeros(size(shiftAlignedKymo, 1));
+    
+    %% align
+    
+    %     Align based on shiftF/shiftB
+    addedPxF = max(abs(shiftF));
+    kymoMaskF = [zeros(size(kymoMask,1), addedPxF) kymoMask zeros(size(kymoMask,1), addedPxF)];
+    alignKymoTemp = [zeros(size(kymoMask,1), addedPxF) unalignedKymo zeros(size(kymoMask,1),addedPxF)];
+    alignedKymoF = alignKymoTemp;
+
+    for i=2:size(alignedKymoF,1)
+        alignedKymoF(i,:) = circshift(  alignKymoTemp(i,:),[0  shiftF(i)]);
+        kymoMaskF(i,:) = circshift(  kymoMaskF(i,:),[0  shiftF(i)]);
+    end
+    
+    addedPxB = max(abs(shiftB));
+    kymoMaskB = [zeros(size(kymoMask,1), addedPxB) kymoMask zeros(size(kymoMask,1), addedPxB)];
+    alignKymoTemp = [zeros(size(kymoMask,1), addedPxB) unalignedKymo zeros(size(kymoMask,1),addedPxB)];
+    alignedKymoB = alignKymoTemp;
+    for i=2:size(alignedKymoB,1)
+        alignedKymoB(end-i+1,:) = circshift(  alignKymoTemp(end-i+1,:),[0 shiftB(end-i+1)]);
+    end
+    toc
+%     figure,imagesc(alignedKymoF);
+%     figure,imagesc(kymoMaskF)
+    figure,imagesc(alignedKymoB)
+    
+% %     %%
+% %     posDif = zeros(1,size(kymoMask,1));
+% %     for i = 1:size(kymoMask,1)
+% %         pF =  shiftF(i);
+% %         pB =  shiftB(i)-shiftB(1);
+% %         posDif(i) = pF-pB;
 % %     end
-%     
-%     
-%     % The number of pixels around each feature on each side (not including
-%     % the pixel the feature is on) on which there may not be another
-%     % feature
+% %     
+% %     trueShift = 0:2:2*size(kymoMask,1)-1;
+% %     
+% %     vals = [shiftF;shiftB-shiftB(1)];
+% %     meanShift = mean([shiftF;shiftB+trueShift(end)]);
+% %     %% if trueshift known:
+% %    f= figure,plot(trueShift-shiftF)
+% %     hold on
+% % plot((trueShift-trueShift(end))-(shiftB))
+% % plot(trueShift-meanShift)
+% % title('Forward/backward Euclidean alignment')
+% % legend({'Forward','Backward','Averaged'})
+% % 
+% % %% otherwise
+% %    f= figure,plot(shiftF)
+% %     hold on
+% % plot(shiftB-shiftB(1))
+% %     meanShift = mean([shiftF;shiftB-shiftB(1)]);
+% % 
+% % plot(meanShift)
+% % title('Forward/backward Euclidean alignment')
+% % legend({'Forward','Backward','Averaged'})
+% %     %%
+% % f= figure,plot(shiftF)
+% % hold on
+% % plot(shiftB)
+% % 
+% % shiftF-shiftF(end)
+    
+%%
+% ignore the first and last pixels
+fgMaskExpansionSideWidth = 10;
+nanvals = sum(kymoMaskF==0);
+st = find(nanvals==0,1,'first');
+stop  = find(nanvals==0,1,'last');
+
+% hyper parameters / set outside
+squareSmoothingWindowLen_pixels = 10;
+blurSigmaWidth_pixels = 2;
+
+
+%   n = ceil(6 *blurSigmaWidth_pixels);
+%   n = n + 1 -mod(n, 2);
+%The laplacian of gaussian is obtained and normalized
+filt = fspecial('log', [2 squareSmoothingWindowLen_pixels],blurSigmaWidth_pixels);
+
+filtImg = imgaussfilt(alignedKymoF(:,st:stop),3);
+
+alignedKymoF(alignedKymoF==0) = nan;
+logim = imfilter(filtImg, filt,nan);
+smoothImg = logim ./ max(abs(logim(:)));
+
+  
+stShifted = st + fgMaskExpansionSideWidth;
+stopShifted = stop - fgMaskExpansionSideWidth;
+
+%% detecting features
 %     W_trim = 5;
-%     typicalFeatureWidth = (2 * W_trim) + 1;
+% depends on camera
+typicalFeatureWidth = 11;
+
+% The maximum number of pixels a feature can move from frame to next
+% frame
+maxFeatureMovementPx = 3;
+maxNumFeaturesSoughtK = ceil((stop-st) / typicalFeatureWidth);
+
+
+mat = smoothImg;%(:, fgMaskExpansionSideWidth:end-fgMaskExpansionSideWidth+1)
+
+% mat = alignedKymoF(:,st:stop);
+% maxNumFeaturesSoughtK = 10;
+
+vec =cell(1,size(mat,1));
+Ws =cell(1,size(mat,1));
+
+for i=1:size(mat,1) % we loose last row
+    [Ypk,Xpk,Wpk] =findpeaks(mat(i,:),'SortStr','descend','NPeaks',maxNumFeaturesSoughtK,'MinPeakDistance',11);
+    vec{i} = Xpk';
+    Ws{i} = Ypk';
+end
+% finding k features
+distPar = 5;
+maxDistFrames = inf;
+
+% track features
+import DBM4.track_sources;
+[sources] = track_sources(vec,distPar,maxDistFrames,Ws);
+
+% consider sources present in half or more frames
+longFeatures = find(cellfun(@(x) size(x,1),sources)>0.9*size(mat,1)-1);
+sourcesLong = sources(longFeatures);
+
+f=figure('Position', [100, 100, 600, 300])
+%  tiledlayout(2,1)
+%  nexttile
+% imagesc(kymo)
+%  nexttile
+hold on
+for s = 1:length(sourcesLong)
+    plot( sourcesLong{s}(:, 2),sourcesLong{s}(:, 1),'|','linewidth', 2)
+end
+% ylim([3.95, 5.05])
+set(gca, 'YDir','reverse')
+
+pathsColIdxs = nan(size(mat,1), length(sourcesLong));
+for i=1:length(sourcesLong)
+    pathsColIdxs(sourcesLong{i}(:,1),i) = sourcesLong{i}(:,2);
+end
+
+for j=1:size(pathsColIdxs,2)
+    % fill up nan's with nearest nonnan
+    m = flipud(pathsColIdxs(:,j));
+    t = ~isnan(m);
+    ii = cumsum(t);
+    ii(ii==0) = 1;
+    ii = bsxfun(@plus, [0 ii(end,1:end-1)],ii);
+
+    % for s=1:size(ii,2)
+    %     ii(:,s) = bsxfun(@plus, [ii(end,s)],ii(:,s));
+    % end
+    m1 = m(t);
+    pathsColIdxs(:,j) = flipud(m1(ii));
+end
+
+% end
+
+img = alignedKymoF(:,st:stop);
+% alignedMask
+
+imgSz = size(img);
+import DBM4.compute_rescale_factors;
+stretchFactorsMat = compute_rescale_factors(pathsColIdxs, imgSz);
+
+
+import OptMap.KymoAlignment.apply_horizontal_stretching;
+alignedKymo = apply_horizontal_stretching(img, stretchFactorsMat);
+alignedMask = round(apply_horizontal_stretching(double(kymoMaskF(:,st:stop)), stretchFactorsMat)) > 0;
+
+
 % 
-%     % The maximum number of pixels a feature can move from frame to next
-%     % frame
-%     maxFeatureMovementPx = 7;
+% imgSz = size(alignedKymoF);
+% import OptMap.KymoAlignment.compute_stretch_factors;
+% stretchFactorsMat = compute_stretch_factors(pathsColIdxs, imgSz);
+
+            
+%%
 % 
-% 
-%     % The number of pixels to ignore at the edges of the kymograph
-%     %  when looking for features
-%     featurelessSideWidth = 0;
-%     % The edges of the region in which features are to be found are
-%     %  specified, unless already done in the call to the function.
-%     leftColIdx = 1 + featurelessSideWidth;
-%     rightColIdx = size(unalignedKymo, 2) - featurelessSideWidth;
-% 
-% 
-%     % The (maximum) number of features to look for "k"
-%     minFgMaskWidth = min(min(sum(shiftAlignedKymoMask, 2)), (size(shiftAlignedKymoMask, 2) - 2*featurelessSideWidth));
-%     maxNumFeaturesSoughtK = ceil(minFgMaskWidth / ((2 * W_trim) + 1));
-% 
-%     
-%     % The shift-aligned kymograph is smoothed out in order to reduce noise
-%     squareSmoothingWindowLen_pixels = 10;
-%     blurSigmaWidth_pixels = 2;
-%     
-%     import OptMap.KymoAlignment.apply_gaussian_blur;
-%     smoothImg = apply_gaussian_blur(shiftAlignedKymo, squareSmoothingWindowLen_pixels, blurSigmaWidth_pixels);
-% 
-% 
-%     %The laplacian of gaussian is obtained and normalized
-%     import OptMap.KymoAlignment.apply_laplacian_of_gaussian_filter;
-%     smoothImg = apply_laplacian_of_gaussian_filter(smoothImg, [2, 6], 2);% blur sigma should depend on nm/px
-%     smoothImg = smoothImg ./ max(abs(smoothImg(:)));
-% 
-%     % why dilate foreground mask?
-% %     fgMaskExpansionSideWidth = 10;
-% %     if not(all(shiftAlignedKymoMask(:)))
-% %         farBgMask = ~imdilate(shiftAlignedKymoMask, true(1, 2*fgMaskExpansionSideWidth + 1));
-% %         smoothImg(farBgMask) = NaN;
-% %     end
-%      smoothImg(~shiftAlignedKymoMask) = NaN;
-%     
-%     alignmentSuccessTF = all(sum(~isnan(smoothImg) & (smoothImg ~= 0), 2));
-%     stretchFactorsMat = ones(size(shiftAlignedKymo));
-%     alignedKymo = shiftAlignedKymo;
-%     alignedMask = shiftAlignedKymoMask;
-%     
-%     
-%     if alignmentSuccessTF
-%         %The k shortest paths through the (prealigned) kymograph are found
-%         import OptMap.KymoAlignment.find_k_features;
-%         pathsColIdxOffsets = find_k_features(smoothImg(:, leftColIdx:rightColIdx), maxFeatureMovementPx, typicalFeatureWidth, maxNumFeaturesSoughtK); 
-%         
-%         alignmentSuccessTF = not(isempty(pathsColIdxOffsets));
-%         if alignmentSuccessTF
-%             pathsColIdxOffsets = horzcat(pathsColIdxOffsets{:, 1});
-%             pathsColIdxs = leftColIdx + pathsColIdxOffsets - 1;
-% 
-%             
-%             % % Force edge alignment -- doesn't currently seem to
-%             % %  behave quite as expected (todo: debug)
-%             % if forceEdgesTF
-%             %     numFrames = size(pathsColIdxs, 1);
-%             %     forcedLeftFeature = NaN(numFrames, 1);
-%             %     forcedRightFeature = NaN(numFrames, 1);
-%             %     for frameNum = 1:numFrames
-%             %         tmp = find(alignedMask(frameNum, :), 1, 'first');
-%             %         if isempty(tmp)
-%             %             tmp = NaN;
-%             %         else
-%             %             tmp = max(1, tmp - edgeFeatureAlignmentBuffer);
-%             %         end
-%             %         forcedLeftFeature(frameNum) = tmp;
-%             % 
-%             %         tmp = find(alignedMask(frameNum, :), 1, 'last');
-%             %         if isempty(tmp)
-%             %             tmp = NaN;
-%             %         else
-%             %             tmp = min(size(smoothImg, 2), tmp + edgeFeatureAlignmentBuffer);;
-%             %         end
-%             %         forcedRightFeature(frameNum) = tmp;
-%             %     end
-%             %     import OptMap.KymoAlignment.nearest_nonnan;
-%             %     forcedLeftFeature = nearest_nonnan(forcedLeftFeature, 1);
-%             %     forcedRightFeature = nearest_nonnan(forcedRightFeature, size(smoothImg, 2));
-%             %     pathsColIdxs = [forcedLeftFeature, pathsColIdxs, forcedRightFeature];
-%             % end
-%             
-%             imgSz = size(alignedKymo);
-%             import OptMap.KymoAlignment.compute_stretch_factors;
-%             stretchFactorsMat = compute_stretch_factors(pathsColIdxs, imgSz);
-% 
-%             import OptMap.KymoAlignment.apply_horizontal_stretching;
-%             alignedKymo = apply_horizontal_stretching(alignedKymo, stretchFactorsMat);
-%             alignedMask = round(apply_horizontal_stretching(double(alignedMask), stretchFactorsMat)) > 0;
-%         end
-%     end
-%     % put nan's on the non-mask of aligned kymo since that's not relevant
-%     % anymore
-%     alignedKymo(~alignedMask) = nan;
-%     % % For debugging reasons, the paths can be drawn in the (unaligned)
-%     % % kymograph.
-%     % 
-%     % 
-%     % 
-%     % szImg = size(unalignedKymo);
-%     % pathLabelsMat = zeroes(szImg);
-%     % numPaths = size(pathsColIdxs, 2);
-%     % pathRowIdxs = 1:szImg(1);
-%     % for pathNum = 1:numPaths
-%     %     pathColIdxs = pathsColIdxs(:, pathNum);
-%     %     pathLabelsMat(sub2ind(pathLinIdxs, pathRowIdxs, pathColIdxs)) = pathNum;
-%     % end
-%     % 
-%     % hFig = figure();
-%     % hPanel = uipanel('Parent', hFig);
-%     % hAxis = axes('Parent', hPanel);
-%     % import OptMap.KymoAlignment.UI.plot_features_overlay;
-%     % plot_features_overlay(hAxis, unalignedKymo, pathLabelsMat);
+%     tic
+% import OptMap.KymoAlignment.find_k_features;
+% pathsColIdxOffsets = find_k_features(smoothImg(:, fgMaskExpansionSideWidth:end-fgMaskExpansionSideWidth+1), maxFeatureMovementPx, typicalFeatureWidth, maxNumFeaturesSoughtK); 
+%   toc
+%   
+% import OptMap.KymoAlignment.apply_laplacian_of_gaussian_filter;
+% smoothImg = apply_laplacian_of_gaussian_filter(alignedKymoF(:,st:stop), [2, squareSmoothingWindowLen_pixels], blurSigmaWidth_pixels);
+% smoothImg = smoothImg ./ max(abs(smoothImg(:)));
+
+% % should erode, not delate, since edges not accurate
+% if not(all(kymoMaskF(:)))
+%     farBgMask = ~imdilate(kymoMaskF, true(1, 2*fgMaskExpansionSideWidth + 1));
+%     smoothImg(farBgMask) = NaN;
+% end
+
+
+    
+    
 end
---
+++ src/MATLAB/+DBM4/track_sources.m
@@ -0,0 +1,161 @@
+function [sources] = track_sources(vec,distPar,maxDistFrames,Wsn)
+
+% from hpfl
+if nargin < 3
+% not usaed really
+maxDistFrames = inf;
+distPar = 5;
+end
+
+if nargin < 4
+   Wsn = vec;
+end
+
+% % source tracking (for regularization or multi-frame fluorophores
+% vec = cell(1,100);
+% for j=1:length(X)
+%     for i=1:size(X{j},1)
+%         vec{X{j}(i,1)} = [vec{X{j}(i,1)} X{j}(i,2)];
+%     end
+% end
+% for i=1:size(X{2},1)
+%     vec{X{2}(i,1)} = [vec{X{2}(i,1)} X{2}(i,2)];
+% end
+
+sources = cell(0,1);
+init = 1;
+while sum(vec{init})==0
+    init=init+1;
+end
+
+for i = 1:length(vec{init})
+    sources{length(sources)+1}=[init,vec{init}(i),Wsn{init}(i)]; % could use intensity
+end
+
+
+
+% distPar=5;
+for l = init+1:length(vec)
+    lastSources =cellfun(@(x) x(end,2),sources);
+    lastSourcesIdx =cellfun(@(x) x(end,1),sources);
+    if~isempty(vec{l})
+        % maybe as first step use match-pairs between the previous time-frame,
+        % and then pdist between the rest (possibly improves accuracy?)
+        dists = (vec{l}'-lastSources').^2;
+    %     A=pdist2(vec{l},lastSources'); % could be within some distance only allowed
+
+        dists(l-lastSourcesIdx>maxDistFrames,:) = inf; % those that are larger than maxDist, don't assign
+        dists(dists>distPar^2)=inf; %todo: adapt so it would be harder to ma over long gaps
+
+            temp=zeros(length(vec{l}),1);
+
+        M = matchpairs(dists,distPar^2/2);
+
+        for a=1:size(M,1)
+            sources{M(a,1)}=[sources{M(a,1)};[l,vec{l}(M(a,2)),Wsn{l}(M(a,2))]];
+        end
+
+        temp(M(:,2))=1;
+
+        for i = 1:length(temp)
+            if temp(i)==0
+                sources{length(sources)+1}=[l,vec{l}(i),Wsn{l}(i)];
+            end
+        end
+    end
+end
+
+
+% 
+% for i=2:100
+%     Xest =vec{i};
+%     Xtrue =vec{2};
+% 
+%     dists = (Xest-Xtrue').^2;
+%     M = matchpairs(dists, 2);
+% 
+% end
+% 
+%     M = matchpairs(dists, localWindowRadius);
+%     
+%     
+% nblamb = length(lambda);
+% sources = cell(0,1);
+% 
+% if nargin <4
+%     distPar = 5; % distance so that features would be assigned to the same
+% end
+% 
+% if nargin < 5
+%     maxDistFrames = inf; % max distance between frames
+% end
+% 
+% init = 1;
+% while sum(X{init})==0
+%     init=init+1;
+% end
+% 
+% for i = 1:length(X{init})
+%     sources{length(sources)+1}=[init,X{init}(i,:),amps{init}(i,:)];
+% end
+% 
+% % distPar=5;
+% for l = init+1:nblamb
+%     lastSources =cellfun(@(x) x(end,2),sources);
+%     lastSourcesIdx =cellfun(@(x) x(end,1),sources);
+%     
+%     % maybe as first step use match-pairs between the previous time-frame,
+%     % and then pdist between the rest (possibly improves accuracy?)
+% 
+%     A=pdist2(X{l},lastSources'); % could be within some distance only allowed
+%     
+%     A(:,l-lastSourcesIdx>maxDistFrames) = inf; % those that are larger than maxDist, don't assign
+%     A(A>distPar)=inf; %todo: adapt so it would be harder to ma over long gaps
+% 
+%     temp=zeros(length(X{l}(:,1)),1);
+%     for a = 1:length(X{l}(:,1)) % go through all previous peaks
+%         [val, n] = min(A(:));
+%         if ~isinf(val)
+%             [x,y] = ind2sub(size(A),n); % x-row/y-column
+%             temp(x)=1; %this row has a match in l
+%             % sources{x} might not hold the correct idx.. if there was some
+%             % shift..
+%             % check all last sources
+% %             lastSources =cellfun(@(x) x(end,2),sources);
+%             % compare to sources from previous step
+% %             [val2,b] = min(pdist2(X{l}(x,:),lastSources')); % this gives an index in sources
+%             %if val2 < distPar
+% %                 xLast=b;
+%                 % now update sources with the new element
+%                 sources{y}=[sources{y};[lambda(l),X{l}(x,:),amps{l}(x,:)]];
+%                 A(x,:)=Inf;
+%                 A(:,y)=Inf;
+% %             else
+% %                 A(x,y)=Inf;
+%         end
+% 
+%     end
+% 
+%     for i = 1:length(temp)
+%         if temp(i)==0
+%             sources{length(sources)+1}=[lambda(l),X{l}(i,:),amps{l}(i,:)];
+%         end
+%     end
+% end
+
+%
+
+% f=figure('Position', [100, 100, 600, 300])
+% %  tiledlayout(2,1)
+% %  nexttile
+% % imagesc(kymo)
+% %  nexttile
+% hold on
+% for s = 1:length(sources)
+%     plot( sources{s}(:, 2),sources{s}(:, 1),'|','linewidth', 2)
+% end
+% % ylim([3.95, 5.05])
+% set(gca, 'YDir','reverse')
+
+end
+
